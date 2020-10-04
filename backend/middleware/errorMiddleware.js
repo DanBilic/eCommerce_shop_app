8b1ae4e140ad939375e543c4f505c6d4a558b141
DanBilic <bilic90@gmx.de>
2020-10-04 23:31:04 +0200

    custom error handling added

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,17 @@
+// fallback handler for 404 errors -> endpoint not found
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_EMV === "production" ? null : err.stack,
+  });
+};
+
+export { notFound, errorHandler };
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
 // import routes
@@ -12,11 +13,13 @@ connectDB();
 
 const app = express();
 
-app.use("/api/products", productRoutes);
-
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
