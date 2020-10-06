722b02a9591b9dfd146c26dd1688c088560f1e3d
DanBilic <bilic90@gmx.de>
2020-10-06 10:34:33 +0200

    added remove item from card action

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 // getState -> get current global state
 export const addToCart = (id, qty) => async (dispatch, getState) => {
@@ -18,5 +18,13 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
   });
 
   // save cart with all items to localstorage
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => async (dispatch, getState) => {
+  // remove cart in gloabal state
+  dispatch({ type: CART_REMOVE_ITEM, payload: id });
+
+  // update the localStorage with the new cartItem state
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
@@ -22,6 +22,13 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           cartItems: [...state.cartItems, item],
         };
       }
+
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        // action.payload is the item id
+        cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+      };
 
     default:
       return state;
